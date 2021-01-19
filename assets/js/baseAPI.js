71dd3293f1aeca88129f90d0c798d728de94b0ee
就这 <18734734863@163.com>
2021-01-19 19:43:28 +0800

    主页功能开发完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,9 @@
 var baseURL = 'http://api-breakingnews-web.itheima.net'
 $.ajaxPrefilter(function(params) {
     params.url = baseURL + params.url
+    if (params.url.indexOf("/my/") !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+$(function() {
+    // 获取用户信息
+    geUserInfo();
+
+    var layer = layui.layer;
+    $("#btnLogout").on('click', function() {
+        layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token');
+            location.href = "/login.html"
+            layer.close(index);
+        });
+    })
+})
+
+function geUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        header: {
+            Authorization: localStorage.getItem("token") || ""
+        },
+        success: function(res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message);
+            }
+
+            rendeAvatar(res.data);
+        }
+    })
+}
+
+function rendeAvatar(user) {
+    var name = user.nickname || user.username;
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        $(".layui-nav-img").hide();
+        var text = name[0].toUpperCase();
+        $(".text-avatat").show().html(text);
+    }
+}
